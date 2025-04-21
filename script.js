5e762bb4ef07858f8b2bb18eedfb8e9331997d9d
Skyred-AK <souss782k@gmail.com>
2025-04-21 19:06:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Exemple simple pour gérer l'ajout au panier
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Article ajouté au panier !');
+    });
+});
